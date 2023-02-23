62200bae68b4b44d0a261216a3f612b3558e81b9
adhyaksazhalifunnas <adhyaksazhalifunnas@gmail.com>
2023-02-23 17:55:28 +0700

    fix(env): fixed env to work with nextjs

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ const nextConfig = {
   images: {
     domains: ["naname-nft-marketplace.infura-ipfs.io", "infura-ipfs.io"],
   },
+  env: {
+    NEXT_PROJECT_ID: process.env.NEXT_PROJECT_ID,
+    NEXT_PROJECT_SECRET_KEY: process.env.NEXT_PROJECT_SECRET_KEY
+  },
 };
 
 module.exports = nextConfig
